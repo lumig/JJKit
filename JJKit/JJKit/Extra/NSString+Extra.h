571c0d4c4dc3321117385dc72e159165bf24bb06
luming <luming@kaixinmahua.local>
2018-06-21 17:02:50 +0800

    push

---
+++ JJKit/JJKit/Extra/NSString+Extra.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Extra.h
+//  JJKit
+//
+//  Created by luming on 2018/6/21.
+//  Copyright © 2018年 luming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSString (Extra)
+
+- (void)lumig;
+
+@end
---
+++ JJKit/JJKit/Extra/NSString+Extra.m
@@ -0,0 +1,18 @@
+//
+//  NSString+Extra.m
+//  JJKit
+//
+//  Created by luming on 2018/6/21.
+//  Copyright © 2018年 luming. All rights reserved.
+//
+
+#import "NSString+Extra.h"
+
+@implementation NSString (Extra)
+
+- (void)lumig
+{
+    NSLog(@"意义");
+}
+
+@end
