77927e68cb3a065d24f0a70b31cab81dc574e31a
luming <luming@kaixinmahua.local>
2018-06-22 10:51:54 +0800

    push

---
+++ JJKit/JJKit/JJKit.h
@@ -0,0 +1,15 @@
+//
+//  JJKit.h
+//  JJKit
+//
+//  Created by luming on 2018/6/22.
+//  Copyright © 2018年 luming. All rights reserved.
+//
+
+#ifndef JJKit_h
+#define JJKit_h
+#ifdef __OBJC__
+#import "NSString+Extra.h"
+
+#endif
+#endif /* JJKit_h */
